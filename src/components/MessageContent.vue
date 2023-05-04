4f52c6806b15ebe1fb08a9654489e3be063c0171
blacktunes <blacktunes@foxmail.com>
2023-05-04 17:01:51 +0800

    feat: 增加通知功能

--- src/components/MessageContent.vue
+++ src/components/MessageContent.vue
@@ -22,7 +22,17 @@
             <template
               #item="{ element, index }: { element: Message, index: number }"
             >
+              <div v-if="element.notice" class="notice">
+                <img src="@/assets/images/通知.png" alt="" />
+                <span contenteditable @input="updateText($event, index)">
+                  {{ element.text }}
+                </span>
+                <div class="del-wrapper">
+                  <div class="notice-del" @click="handelDelClick(index)">×</div>
+                </div>
+              </div>
               <div
+                v-else
                 class="message"
                 :class="{
                   right: element.key === '开拓者',
@@ -60,7 +70,7 @@
       </div>
     </div>
     <div class="menu">
-      <div class="btn" @click="handelSelectClick">
+      <div class="btn" @click="handelSelectClick" title="选择角色">
         <img :src="getAvatar(input.character)" alt="" />
       </div>
       <input
@@ -69,7 +79,7 @@
         v-model="input.input"
         @keydown.enter="handelAddClick()"
       />
-      <div class="btn" @click="handelImageClick">
+      <div class="btn" @click="handelImageClick" title="发送图片">
         <svg
           viewBox="0 0 1024 1024"
           version="1.1"
@@ -88,7 +98,21 @@
           ></path>
         </svg>
       </div>
-      <div class="btn" style="margin-left: 0" @click="handelAddClick()">
+      <div class="btn" @click="handelNoticeClick" title="发送通知">
+        <svg
+          viewBox="0 0 1024 1024"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          width="70"
+          height="70"
+        >
+          <path
+            d="M889.828994 749.637494c-1.204099-1.548127-119.205779-166.165631-119.205779-287.435579 0-168.401814-76.202251-254.752898-162.725349-286.919536L607.897867 159.973123c0-52.980346-43.175542-95.983874-95.983874-95.983874S415.930119 107.164791 415.930119 159.973123l0 16.685369c-10.836889 7.912649-16.169326 21.845792-11.868974 35.262893 5.332437 16.857383 23.393919 25.974131 40.251302 20.641693 17.717453-5.676466 36.983034-8.428691 56.936671-8.428691l21.32975 0c115.249454 0 184.055098 89.103309 184.055098 238.239543 0 108.024861 72.933983 235.315303 110.261045 293.456073-57.452713 26.146145-183.023014 76.202251-304.809004 76.202251-121.441962 0-247.356291-50.400134-304.809004-76.546279 37.327062-57.968755 110.089031-184.915169 110.089031-293.112044 0-62.441122 11.524945-114.217369 34.574836-153.952629 8.77272-15.309256 3.612296-34.918864-11.69696-43.691584-15.48127-8.944734-34.918864-3.612296-43.691584 11.69696-28.554342 49.540064-43.175542 112.1532-43.175542 185.947253 0 121.269948-118.00168 285.199395-119.205779 286.919536-5.332437 7.568621-7.396607 16.857383-5.332437 25.974131 2.064169 8.944734 7.912649 16.685369 15.997312 20.985721 5.676466 3.096254 107.336805 57.280699 233.423148 84.458928 21.32975 66.397447 73.966068 111.29313 133.654964 111.29313s112.325214-44.895683 133.654964-111.29313c126.086343-27.178229 227.746682-81.018646 233.423148-83.942886 8.084663-4.472367 13.933143-12.040988 16.169326-20.985721C897.225601 766.666891 895.333445 757.206115 889.828994 749.637494zM501.249118 159.973123c-7.224593 0-14.277171 1.032085-21.32975 1.548127L479.919368 159.973123c0-17.545439 14.449185-31.994625 31.994625-31.994625 17.545439 0 31.994625 14.449185 31.994625 31.994625l0 0.860071c-7.224593-0.516042-14.277171-0.860071-21.32975-0.860071L501.249118 159.973123zM511.913993 928.016126c-22.361834 0-43.175542-13.417101-57.968755-35.262893 19.26558 2.064169 38.531161 3.268268 57.968755 3.268268s38.875189-1.376113 57.968755-3.268268C555.089535 914.599026 534.275827 928.016126 511.913993 928.016126z"
+            fill="#575B66"
+          ></path>
+        </svg>
+      </div>
+      <div class="btn" @click="handelAddClick()" title="发送消息">
         <svg
           viewBox="0 0 1024 1024"
           version="1.1"
@@ -268,6 +292,19 @@ const handelImageClick = () => {
   el.click()
 }
 
+const handelNoticeClick = () => {
+  message.list[index.value].list.push({
+    key: '开拓者',
+    name: '',
+    avatar: '',
+    text: input.input || '愿此行，终抵群星',
+    notice: true
+  })
+  message.list[index.value].time = Date.now()
+  input.input = ''
+  scrollToBottom()
+}
+
 const handelAddClick = (img?: string) => {
   message.list[index.value].list.push({
     key: input.character,
@@ -399,6 +436,50 @@ $width = 2000px
       margin 30px 60px 30px 50px
       padding-right 50px
 
+      .notice
+        display flex
+        justify-content center
+        align-items center
+        width 100%
+        height 60px
+        padding 90px 0
+        font-size 30px
+        color #949595
+
+        &:hover
+          background #ddd
+
+          .del-wrapper
+            .notice-del
+              opacity 1
+
+        span
+          max-width 80%
+          margin-left 20px
+          overflow hidden
+          white-space nowrap
+          text-overflow ellipsis
+
+        .del-wrapper
+          position relative
+          height 100%
+
+          .notice-del
+            display flex
+            align-items center
+            justify-content center
+            position absolute
+            right -80px
+            top -5px
+            width 60px
+            height 60px
+            font-size 60px
+            opacity 0
+            cursor pointer
+
+            &:hover
+              opacity 1
+
       .message
         display flex
         height fit-content
@@ -441,7 +522,7 @@ $width = 2000px
               justify-content center
               position absolute
               right -100px
-              bottom -8px
+              bottom -6px
               width 80px
               height 80px
               font-size 50px
@@ -503,6 +584,7 @@ $width = 2000px
   height 150px
   border-top 5px solid #b3b3b3
   background #c7c8ca
+  padding 0 10px
 
   .btn
     display flex
@@ -522,6 +604,7 @@ $width = 2000px
   .input
     flex 1
     height 100px
+    margin 0 10px
     background #e8e8e8
     font-size 50px
     text-align center
