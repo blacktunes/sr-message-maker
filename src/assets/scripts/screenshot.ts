4413949a6b9946f228b642f1695bda64a4d24da5
blacktunes <blacktunes@foxmail.com>
2023-08-09 13:31:14 +0800

    perf: 优化图片保存机制

--- src/assets/scripts/screenshot.ts
+++ src/assets/scripts/screenshot.ts
@@ -1,39 +1,21 @@
-import { getFontEmbedCSS, toBlob, toPng } from 'html-to-image'
+import { getFontEmbedCSS, toPng } from 'html-to-image'
 
 export const screenshot = async (dom: HTMLElement, width?: number, height?: number) => {
   try {
     const fontEmbedCSS = await getFontEmbedCSS(dom)
     const title = `SR-${Date.now()}`
-    if (import.meta.env.MODE === 'development') {
-      const dataUrl = await toPng(dom, {
-        width,
-        height,
-        fontEmbedCSS
-      })
-      const img = new Image()
-      img.src = dataUrl
-      img.alt = title
-      const win = window.open('')
-      if (win) {
-        win.document.title = title
-        win.document.body.appendChild(img)
-      }
-    } else {
-      const blob = await toBlob(dom, {
-        width,
-        height,
-        fontEmbedCSS
-      })
-      if (blob) {
-        const url = URL.createObjectURL(blob)
-        const link = document.createElement('a')
-        link.setAttribute('download', `${title}.png`)
-        link.setAttribute('href', url)
-        link.click()
-
-        link.remove()
-        URL.revokeObjectURL(url)
-      }
+    const dataUrl = await toPng(dom, {
+      width,
+      height,
+      fontEmbedCSS
+    })
+    const img = new Image()
+    img.src = dataUrl
+    img.alt = title
+    const win = window.open('')
+    if (win) {
+      win.document.title = title
+      win.document.body.appendChild(img)
     }
   } catch (error) {
     console.error('截图保存错误', error)
