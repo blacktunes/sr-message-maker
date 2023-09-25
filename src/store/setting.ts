f307d0a7dec2a71e12e3386b6d65db9b2f481cb2
blacktunes <blacktunes@foxmail.com>
2023-09-25 13:40:06 +0800

    fix: 修复字符串解析错误导致的白屏

--- src/store/setting.ts
+++ src/store/setting.ts
@@ -26,7 +26,11 @@ export const setAvatar = (key: string | number = DEFAULT_AVATAR) => {
 }
 
 setting.name = localStorage.getItem('sr-message-name') || '开拓者'
-setting.avatar = JSON.parse(localStorage.getItem('sr-message-avatar') ?? JSON.stringify(DEFAULT_AVATAR)) 
+try {
+  setting.avatar = JSON.parse(localStorage.getItem('sr-message-avatar') || '')
+} catch {
+  setting.avatar = DEFAULT_AVATAR
+}
 setting.bubbles = Number(localStorage.getItem('sr-message-bubbles')) || 0
 
 export const autoPlaySetting = reactive<{
