89c84e2813d3d2259f6ee8ab14e8c8f746ca423f
blacktunes <blacktunes@foxmail.com>
2023-12-10 17:06:38 +0800

    feat: 增加GIF动图支持

--- src/assets/scripts/image.ts
+++ src/assets/scripts/image.ts
@@ -1,22 +1,30 @@
 export const compressImage = (file: File | Blob, width?: number) => {
   return new Promise<string>((reslove) => {
-    const src = URL.createObjectURL(file)
-    const img = new Image()
-    img.src = src
-    img.onload = () => {
-      const canvas = document.createElement('canvas')
-      const ctx = canvas.getContext('2d')
-      if (!ctx) {
-        reslove('')
-        return
+    if (file.type === 'image/gif') {
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        reslove((e.target?.result as string) || '')
       }
+      reader.readAsDataURL(file)
+    } else {
+      const src = URL.createObjectURL(file)
+      const img = new Image()
+      img.src = src
+      img.onload = () => {
+        const canvas = document.createElement('canvas')
+        const ctx = canvas.getContext('2d')
+        if (!ctx) {
+          reslove('')
+          return
+        }
 
-      width = width ? (img.width < width ? img.width : width) : img.width
-      canvas.width = width
-      canvas.height = width * (img.height / img.width)
-      ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
-      reslove(canvas.toDataURL('image/webp'))
-      URL.revokeObjectURL(src)
+        width = width ? (img.width < width ? img.width : width) : img.width
+        canvas.width = width
+        canvas.height = width * (img.height / img.width)
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+        reslove(canvas.toDataURL('image/webp'))
+        URL.revokeObjectURL(src)
+      }
     }
   })
 }
